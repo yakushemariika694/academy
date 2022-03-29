bd17a2174f6e275c880b289c1aa57a9195838580
yakushemariika694 <102573698+yakushemariika694@users.noreply.github.com>
2022-03-29 08:08:53 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,20 @@
+onst solanaJSON = require('./solana-json.js');
+
+(async() => {
+  console.log('deploying...');
+  const connection = solanaJSON.setupConnection('https://testnet.solana.com');
+  const payerAccount = await solanaJSON.createUser();
+  await solanaJSON.fundUser(connection,payerAccount);
+  
+  const smartContract = {
+    pathToProgram: './solana-json.so',
+    dataLayout: solanaJSON.setDataStructure(1000),
+  }
+
+  const app = await solanaJSON.loadProgram(connection,smartContract,payerAccount);
+  console.log('app',app);
+
+  const confirmationTicket = await solanaJSON.pushJSON(connection,app,'{"abc":123}');
+  const testJSON = solanaJSON.pullJSON(connection,app.appAccount.publicKey);
+  console.log(`Test: ${JSON.parse(testJSON).abc}`);
+})();
